a35204ac74453dbd1987bc07ed33c537e9d413a7
agenciakreativ <falecomkreativ@gmail.com>
2026-02-02 12:37:31 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neon-lime': '#a3e635',
+        'neon-fuchsia': '#e879f9',
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+  plugins: [],
+}
